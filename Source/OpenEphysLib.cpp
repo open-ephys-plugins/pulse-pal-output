fcaa9b26e0af9ad34a22d3187a0297a4769d011f
Anjal Doshi <anjal.doshi@alleninstitute.org>
2025-07-15 10:54:23 -0700

    Bump plugin version to 1.0.0

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -38,7 +38,7 @@ extern "C" EXPORT void getLibInfo (Plugin::LibraryInfo* info)
 {
     info->apiVersion = PLUGIN_API_VER;
     info->name = "Pulse Pal Output";
-    info->libVersion = "0.2.0";
+    info->libVersion = "1.0.0";
     info->numPlugins = NUM_PLUGINS;
 }
 
